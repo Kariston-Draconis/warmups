41f81be86b3cd51c31a2ca3c8f6a7a79404d3f4b
Alex May <thrawnrulz@hotmail.com>
2017-05-31 10:05:20 +1000

    loop warmups

---
+++ loop-warmup/loopwarmup.js
@@ -0,0 +1,37 @@
+console.log('loop warmups')
+
+for (var year = 1917; year <=2017; year++) {
+  if (year % 4 === 0) {
+    console.log(year);
+  }
+}
+
+console.log('----------------')
+console.log('----------------')
+console.log('----------------')
+
+for (var num = 0; num <= 200;  num++) {
+  if (num % 7 === 0) {
+    console.log(num)
+  }
+}
+
+console.log('----------------')
+console.log('----------------')
+console.log('----------------')
+
+for (var odd = 0; odd <= 200; odd++) {
+ if (odd % 2 !== 0) {
+  console.log(odd)
+  }
+}
+
+console.log('----------------')
+console.log('----------------')
+console.log('----------------')
+
+for (var prime = 0; prime <= 200; prime++) {
+ if (prime ) {
+  console.log(prime)
+  }
+}
